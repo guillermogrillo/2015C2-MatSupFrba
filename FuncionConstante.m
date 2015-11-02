d4634e085818e500aa4b1d833ddbfafa8efba4ed
Raul <raulcubitto@gmail.com>
2015-11-02 00:05:57 -0300

    Fix en funcion constante. Quita intervalos

--- FuncionConstante.m
+++ FuncionConstante.m
@@ -1,25 +1,11 @@
-function [ y ] = FuncionConstante( t, inf, sup, const )
+function [ y ] = FuncionConstante( t, const )
 
     % Chequea cantidad de parametros y completa con defaults
-    switch nargin
-        case 1
-            inf = 0;
-            sup = 2;
-            const = 1;
-        case 2
-            sup = 2;
-            const = 1;
-        case 3
-            const = 1;
+    if (nargin == 1)
+        const = 1;
     end
     
-    % Suponemos que t esta entre (inf;sup)
-    y = const;
-
-    % Si t no esta entre (inf;sup) entonces la funcion vale cero
-    if ( t <= inf || t >= sup )
-        y = 0;
-    end
+    y = const
     
 end
 
