4a7117348b33d45b2d20893021a738c097d3110b
damianvc31 <damiancohen93@gmail.com>
2015-11-02 10:36:05 -0300

    Update FuncionModulo.m

--- FuncionModulo.m
+++ FuncionModulo.m
@@ -1,6 +1,14 @@
-function [ y ] = FuncionModulo( t )
+function [ y ] = FuncionModulo( t, a, b )
+    
+    switch nargin
+        case 1  
+            a = 1;
+            b = 0;
+        case 2
+            b = 0;
+    end
 
-    y = abs(t);
+    y = abs(a * t + b);
     
 end
 
